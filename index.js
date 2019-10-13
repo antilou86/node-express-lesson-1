bc82db16ff01f7698655b44895940ae7a8a1cf29
antilou86 <antilou86@gmail.com>
2019-10-13 13:28:15 -0400

    added endpoints for CRUD

--- index.js
+++ index.js
@@ -2,10 +2,24 @@
 const port = 5000;
 
 const server = express();
+server.use(express.json());
 
 server.get('/hobbitses', (req, res) => {
     res.send('Hello precious hobbitses');
 });
+
+server.post('/hobbitses', (req, res) => {
+    res.status(201).json({url: '/hobbitses', operation: 'POST'});
+});
+
+server.put('/hobbitses', (req, res) => {
+    res.status(200).json({url: '/hobbitses', operation: 'put'});
+});
+
+server.delete('/hobbitses', (req, res) => {
+    res.status(204);
+});
+
 
 server.listen(port, hostname, () => {
     console.log(`server listening on ${port}`);
